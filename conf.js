8b2478bea44d754ce9483505d67f9a6f1857e05f
plorry <timebot@gmail.com>
2014-12-16 22:07:42 -0500

    initial commit

---
+++ conf.js
@@ -0,0 +1,13 @@
+var Images = exports.Images = {
+    bike_lane: './assets/bike_lane.png',
+    bg_toronto: './assets/toronto.jpg',
+    biker: './assets/bike_test.png',
+    cityhall: './assets/city_hall.png',
+    hHouse01: './assets/house01.png',
+    shrub01: './assets/shrub01.png',
+    tree01: './assets/tree01.png'
+};
+
+var globals = exports.globals = {
+    fps: 30
+};
